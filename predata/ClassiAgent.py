42e84cb6eea6e3020619ba0e2cfb1402cb3f57c8
11lookpig23 <mengjie@shanghaitech.edu.cn>
2024-11-27 21:14:50 +0800

    Add files via upload

--- predata/ClassiAgent.py
+++ predata/ClassiAgent.py
@@ -1,33 +1,23 @@
 import numpy as np
-from utils.utils import process_and_compare_predictions, load_data, sample_test_nodes
+from utils.utils import load_data
 import sys
 import requests
 requests.packages.urllib3.disable_warnings()
 import yaml
 import json
 from txtai.embeddings import Embeddings
-import pickle
-from Retriever import dp
-import time
-import random
 
 
-class linkInContextAgent:
+class agent_classify:
     def __init__(self,parafile):
         # read YAML
         with open(parafile, 'r') as file:
             parameters = yaml.safe_load(file)
         ssize = parameters['samplesize']
-        self.test = parameters['testsize']
         self.load_embedding = parameters['load_embedding']
         self.dataname = parameters['dataset_name']
-        self.LLM = parameters['LLM']
-        self.maxp1 = parameters['max_papers'][0]
-        self.maxp2 = parameters['max_papers'][1]
         self.abs_len = parameters['abstract_len']
         self.shotnum = parameters['fewshot_num']
-        self.method = parameters['method']
-        self.style = parameters['style']
         ## create embed
 
         ## load data
@@ -38,32 +28,19 @@ class linkInContextAgent:
             self.sample_size = ssize
         ##
         idx_list = list(range(self.sample_size))
-        node_indices = sample_test_nodes(self.data, self.text, self.sample_size, self.dataname)
+        node_indices = self.sample_test_nodes(self.data, self.text, self.sample_size, self.dataname)
         self.node_index_list = [node_indices[idx] for idx in idx_list]
 
         if parameters['emb'][0]:
             self.creatembed(parameters['emb'][1])
         else:
             self.embeddings = None
-        
-        self.resources = {'data':self.data,'text':self.text,'node_list':self.node_index_list,'embeddings':self.embeddings}
-    def loadjson(self,path):
-        with open(path, 'r') as f:
-            fi = json.load(f)
-        return fi
-
-    def dumppkl(self,fname,res,folder = 'results/predictresult/'):
-        with open(folder+fname, 'wb') as file:
-            pickle.dump(res, file,pickle.HIGHEST_PROTOCOL)
-    def dumpjson(self,fname,res,folder = 'results/predictresult/'):
-        with open(folder+fname,"w") as f:
-            json.dump(res,f)
+            
+            
     def logger(self,experName):
         self.experName = experName
-        #self.jsonfile = open('jsonfile/'+self.dataname+"__"+self.experName+'.jsonl','a')
         self.f = open('logresult/'+self.dataname+"__"+self.experName+'.log', 'a')
         sys.stdout = self.f
-        print("experName .... ",self.experName)
 
     def creatembed(self,index_file):
         if self.load_embedding:
@@ -82,20 +59,35 @@ class linkInContextAgent:
         # Index the list of te
         embeddings.index(alltxt)
         return embeddings
+    
+    
+    def sample_test_nodes(self,data, text, sample_size):
+        """
+        Parameters:
+            data:  data object.
+            text: Textual information associated with nodes.
+            sample_size (int): Number of test nodes to sample.
+            dataset (str): Name of the dataset being used.
+        """
 
-    def constructExamples(self,raw_exam_file):
-        if self.method == "citation":
-            with open(raw_exam_file, 'rb') as inp:
-                examples = pickle.load(inp)
-            return examples
-        examples = {}
-        with open(raw_exam_file, 'r') as inp:
-            raw_exam= json.load(inp)
+        np.random.seed(42)
+        test_indices = np.where(data.test_mask.numpy())[0]
 
-        if self.method == "cluster":
-            for key,val in raw_exam.items():
-                examples[int(key)] = [dp(v,-1,self.has_label(key)) for v in val]
-        else:
-            for key,val in raw_exam.items():
-                examples[int(key)] = [dp(v,-1,self.has_label(key)) for v in val[1:self.shotnum+1]]
-        return examples
+        # Sample 2 times the sample size
+        # node_indices = sample_test_nodes(data, 2 * sample_size)
+        sampled_indices_double = np.random.choice(test_indices, size=2*sample_size, replace=False)
+
+        # Filter out the indices of nodes with title "NA\n"
+        sampled_indices = [node_idx for i, node_idx in enumerate(sampled_indices_double) 
+                    if text['title'][node_idx] != "NA\n"]
+        sampled_indices = sampled_indices[:sample_size]
+
+        # sanity check
+        count = 0
+        for node_idx in sampled_indices:
+            if text['title'][node_idx] == "NA\n":
+                count += 1
+        assert count == 0
+        assert len(sampled_indices) == sample_size
+
+        return sampled_indices
